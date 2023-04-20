67e70eac909691b90a1b757a2846f749e4931f5d
Keid Achird <keidachird@gmail.com>
2023-04-21 01:48:21 +0300

    Add general styles for page

--- src/index.js
+++ src/index.js
@@ -3,21 +3,24 @@ import './scss/main.scss'
 import { getAnimalType, setAnimalType } from './js/utils/localStorage'
 import { getAnimals, getAnimal } from './js/service/service'
 
-const [animalsEl, catBtn, dogBtn] = ['.animals', '.cat-btn', '.dog-btn'].map(
-  selector => document.querySelector(selector)
-)
+const [animalsEl, catBtn, dogBtn, backgroundEl] = [
+  '.animals',
+  '.btn--cat',
+  '.btn--dog',
+  '.background',
+].map(selector => document.querySelector(selector))
 
 // DOM manipulations
 const createHtmlNode = (tag, className, textContent = '') => {
   const node = document.createElement(`${tag}`)
-  node.classList.add(`${className}`)
+  node.classList = className
   node.textContent = textContent
   return node
 }
 
 const renderAnimals = animals => {
   const animalEls = animals.reduce((arr, animal) => {
-    const animalEl = createHtmlNode('button', 'animal', animal.name)
+    const animalEl = createHtmlNode('button', 'btn animal', animal.name)
     animalEl.addEventListener('click', () =>
       handleAnimalClick(animal.id, animal.species)
     )
@@ -25,10 +28,31 @@ const renderAnimals = animals => {
   }, [])
   animalsEl.innerHTML = ''
   animalsEl.append(...animalEls)
+
+  const animalType = getAnimalType()
+
+  // Manipulate CSS styles
+  // TODO: make a separate function
+  if (animalType === 'cat') {
+    catBtn.classList.add('btn--active')
+    dogBtn.classList.remove('btn--active')
+  } else {
+    dogBtn.classList.add('btn--active')
+    catBtn.classList.remove('btn--active')
+  }
+
+  if (backgroundEl.classList.value === 'background') {
+    backgroundEl.classList.add(`background--${getAnimalType()}`)
+  } else {
+    backgroundEl.classList.toggle('background--cat')
+    backgroundEl.classList.toggle('background--dog')
+  }
 }
 
 // Event handlers
 const handleTypeChange = async type => {
+  if (type === getAnimalType()) return
+
   setAnimalType(type)
   const animals = await getAnimals(getAnimalType())
   renderAnimals(animals)
