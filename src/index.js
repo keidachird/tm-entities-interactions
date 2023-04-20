5e39308d2ae2672fcc16fdfb1727780ae523180c
Keid Achird <keidachird@gmail.com>
2023-04-20 22:44:43 +0300

    Move getAnimals and getAnimal functions to service.js, refactor enviroment variables usage

--- src/index.js
+++ src/index.js
@@ -1,37 +1,13 @@
 import './scss/main.scss'
 
-import Dog from './js/classes/Dog'
-import Cat from './js/classes/Cat'
-
 import { getAnimalType, setAnimalType } from './js/utils/localStorage'
-import { getData } from './js/service/service'
+import { getAnimals, getAnimal } from './js/service/service'
 
 const [animalsEl, catBtn, dogBtn] = ['.animals', '.cat-btn', '.dog-btn'].map(
   selector => document.querySelector(selector)
 )
 
-const API_URL_CAT = process.env.API_URL_CAT
-const API_KEY_CAT = process.env.API_KEY_CAT
-const API_URL_DOG = process.env.API_URL_DOG
-const API_KEY_DOG = process.env.API_KEY_DOG
-
-const getAnimals = async animalType => {
-  const apiUrl = animalType === 'cat' ? API_URL_CAT : API_URL_DOG
-  const animalsJson = await getData(`${apiUrl}/breeds`)
-  const animalsList = animalsJson.map(data =>
-    animalType === 'cat' ? new Cat(data) : new Dog(data)
-  )
-  return animalsList
-}
-
-const getAnimal = async (animalId, animalType) => {
-  const apiUrl = animalType === 'cat' ? API_URL_CAT : API_URL_DOG
-  const animalJson = await getData(`${apiUrl}/breeds/${animalId}`, API_KEY_CAT)
-  const animal =
-    animalType === 'cat' ? new Cat(animalJson) : new Dog(animalJson)
-  return animal
-}
-
+// DOM manipulations
 const createHtmlNode = (tag, className, textContent = '') => {
   const node = document.createElement(`${tag}`)
   node.classList.add(`${className}`)
@@ -51,15 +27,16 @@ const renderAnimals = animals => {
   animalsEl.append(...animalEls)
 }
 
+// Event handlers
 const handleTypeChange = async type => {
   setAnimalType(type)
   const animals = await getAnimals(getAnimalType())
-  console.log(animals)
   renderAnimals(animals)
 }
 
 const handleAnimalClick = async (id, type) => {
   const selectedAnimal = await getAnimal(id, type)
+  await selectedAnimal.loadPreviewImage()
   console.log(selectedAnimal)
 }
 
@@ -68,24 +45,7 @@ class App {
     catBtn.addEventListener('click', async () => await handleTypeChange('cat'))
     dogBtn.addEventListener('click', async () => await handleTypeChange('dog'))
 
-    const animalType = await getAnimalType()
-    const onLoadAnimals = await getAnimals(animalType)
-    renderAnimals(onLoadAnimals)
-
-    console.log(animalsEl)
-
-    // TODO implement animal preview image
-    // const catImgPromises = await catsList.map(cat => {
-    //   if (cat.referenceImageId) {
-    //     const img = getData(API_URL + `/images/${cat.referenceImageId}`)
-    //     return img
-    //   }
-    //   return {}
-    // })
-    // const catImgs = (await Promise.all(catImgPromises)).map(
-    //   promise => promise.url
-    // )
-    // console.log(catImgs)
+    renderAnimals(await getAnimals())
   }
 }
 
--- src/js/service/service.js
+++ src/js/service/service.js
@@ -1,9 +1,51 @@
 import axios from 'axios'
 
-export const getData = async (url, options = {}) => {
+import Cat from '../classes/Cat'
+import Dog from '../classes/Dog'
+import { getAnimalType } from '../utils/localStorage'
+
+const getApiUrl = () =>
+  getAnimalType() === 'cat' ? process.env.API_URL_CAT : process.env.API_URL_DOG
+const getApiKey = () =>
+  getAnimalType() === 'cat' ? process.env.API_KEY_CAT : process.env.API_KEY_DOG
+
+export const getData = async url => {
+  const headers = {
+    'Content-Type': 'application/json',
+    'x-api-key': getApiKey(),
+  }
+
   try {
-    const response = await axios.get(url, options)
+    const response = await axios.get(url, { headers })
     return response.data
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const getAnimals = async () => {
+  const animalType = getAnimalType()
+  const apiUrl = getApiUrl()
+
+  try {
+    const animalsJson = await getData(`${apiUrl}/breeds`)
+    const animalsList = animalsJson.map(data =>
+      animalType === 'cat' ? new Cat(data) : new Dog(data)
+    )
+    return animalsList
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const getAnimal = async (animalId, animalType) => {
+  const apiUrl = getApiUrl()
+
+  try {
+    const animalJson = await getData(`${apiUrl}/breeds/${animalId}`)
+    const animal =
+      animalType === 'cat' ? new Cat(animalJson) : new Dog(animalJson)
+    return animal
   } catch (error) {
     console.log(error)
   }
